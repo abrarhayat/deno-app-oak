b738132454a87b4064c7a979352c3b892c917020
Abrar Hayat <abrarhayat@gmail.com>
2021-02-08 00:27:42 +0600

    Added cors headers

--- app.ts
+++ app.ts
@@ -5,6 +5,17 @@ import todosRoutes from "./routes/todo.ts";
 const app = new Application();
 
 app.use(async (ctx, next) => {
+  const responseHeaders = ctx.response.headers;
+  responseHeaders.set("Access-Control-Allow-Origin", "*");
+  responseHeaders.set(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, DELETE"
+  );
+  responseHeaders.set("Access-Control-Allow-Headers", "Content-Type");
+  await next();
+});
+
+app.use(async (ctx, next) => {
   console.log("This always runs!");
   await next(); //if any of our other middlewares have async tasks we must put await for all middleware, otherwise
   //a response will be sent before the async tasks are finsihed
@@ -13,4 +24,5 @@ app.use(async (ctx, next) => {
 app.use(todosRoutes.routes());
 app.use(todosRoutes.allowedMethods());
 
-await app.listen({ port: 8000 });
+const PORT = parseInt(Deno.env.get("PORT") as string) || 8000;
+await app.listen({ port: PORT });
